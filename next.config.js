6502e9ef93e9900af42879873b0d9e4165872ab3
snehithapothina <pothinasnehasai@1729@gmail.com>
2024-11-21 19:42:51 -0600

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+// const path = require("path");
+
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "src", "styles")],
+//   },
+// };
+
+
 const path = require("path");
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src", "styles")],
   },
+  output: "export", // Enable static HTML export
 };
+
+module.exports = nextConfig;
